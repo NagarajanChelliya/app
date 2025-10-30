9aa951568b0f2006ad4b5a0472393520a78a8649
Nagarajan <nagarajan.chelliya@gmail.com>
2025-10-30 22:22:46 +0000

    asdfasd

--- src/main/java/com/myorg/AppApp.java
+++ src/main/java/com/myorg/AppApp.java
@@ -21,7 +21,7 @@ public class AppApp {
 
                            //     .build());
                new AppStack(app, "AppStack");
-              //new PipelineStack(app, "PipelineStack");
+              new PipelineStack(app, "PipelineStack");
                /*
                new PipelineStack(app, "PipelineStack", StackProps.builder()
         .env(Environment.builder().account("242332171291").region("us-east-1").build())
